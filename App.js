086210c59e1685a05032ec1e1ae59671f5cdb975
Acaraccioli <caraccioli_24@hotmail.com>
2018-12-16 23:32:11 -0600

    redes sociales

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // @flow
 import * as React from "react";
-import {StatusBar, Platform} from "react-native";
+import {StatusBar, Platform,   TouchableHighlight} from "react-native";
 import {createSwitchNavigator, createAppContainer} from "react-navigation";
 import {Font, AppLoading} from "expo";
 
@@ -13,6 +13,8 @@ import {MusicNavigator} from "./src/music";
 import {PhotographyNavigator} from "./src/photography";
 import {TravelNavigator} from "./src/travel";
 import {PlayerProvider} from "./src/components/music";
+//social
+import {  StyleSheet, Text, View, Button, Linking, Image } from "react-native";
 
 // $FlowFixMe
 const SFProTextBold = require("./fonts/SF-Pro-Text-Bold.otf");
@@ -57,6 +59,10 @@ export default class App extends React.Component<AppProps, AppState> {
     }
 
     render(): React.Node {
+           
+
+
+
         const {isReady} = this.state;
         if (!isReady) {
             return (
@@ -71,7 +77,10 @@ export default class App extends React.Component<AppProps, AppState> {
             );
         }
         return (
+
+
             <React.Fragment>
+
                 <StatusBar
                     translucent
                     backgroundColor="transparent"
@@ -82,10 +91,80 @@ export default class App extends React.Component<AppProps, AppState> {
                         <AppNavigator {...{onNavigationStateChange}} />
                     </PlayerProvider>
                 </ThemeProvider>
+    
+
+
+
+
+
+
+
+    
+
+<View style={{flexDirection: "row",      backgroundColor: 'lightblue'
+  , justifyContent: 'space-evenly'
+}}>
+
+    <TouchableHighlight onPress={() => Linking.openURL("https://www.facebook.com/RutaCincoHn/")}>
+      <Image
+        style={styles.button}
+        source={require('./facebook.png')}
+      />
+    </TouchableHighlight>
+
+    <TouchableHighlight onPress={() => Linking.openURL("https://mobile.twitter.com/ruta5hn")}>
+      <Image
+        style={styles.button}
+        source={require('./twitter.png')}
+      />
+    </TouchableHighlight>
+
+     <TouchableHighlight onPress={() => Linking.openURL("https://www.instagram.com/ruta5hn/")}>
+      <Image
+        style={styles.button}
+        source={require('./instagram.png')}
+      />
+    </TouchableHighlight>
+
+     <TouchableHighlight onPress={() => Linking.openURL("https://www.linkedin.com/in/ruta5hn/")}>
+      <Image
+        style={styles.button}
+        source={require('./link.png')}
+      />
+    </TouchableHighlight>
+
+     <TouchableHighlight onPress={() => Linking.openURL("https://www.youtube.com/channel/UCk_-JJq-7Pv7W-IfqiyWnvg/featured")}>
+      <Image
+        style={styles.button}
+        source={require('./youtube.png')}
+      />
+    </TouchableHighlight>
+
+               
+       </View>
+
+
             </React.Fragment>
         );
     }
 }
+
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop:10,
+    flexDirection: 'row',
+    justifyContent: 'space-evenly'
+  },
+  button: {
+    backgroundColor: 'lightblue',
+    width: 40,
+    height: 40
+  }
+});
+
 
 const MainNavigator = createSwitchNavigator({
     Welcome: { screen: Welcome },
